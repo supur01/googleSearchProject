465852d5c7bc73987fa3b02239d6d5de32927963
mustafa <supur@ualberta.ca>
2022-01-13 13:16:26 -0700

    googleSearchProject for Jenkins

--- src/test/java/org/supra/runners/CukesRunner.java
+++ src/test/java/org/supra/runners/CukesRunner.java
@@ -7,13 +7,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html"},
+        plugin = {"json:target/cucumber.json",
+                "html:target/cucumber/",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt"
+        },
         features = "src/test/resources/features",
-        glue = "org/supra/step_definitions",
-        tags = "@googleMultiple",
-        dryRun = false,
-        publish = true
+        glue = "org/supra/step_definitions"
+
+
 )
 public class CukesRunner {
 
 }
+
